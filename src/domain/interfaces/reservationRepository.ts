2e262be8a579622f3e6d6ef6dc8cc16bd13442eb
AnderMichael <andercaymi@gmail.com>
2023-11-20 08:39:30 -0400

    feat: get reservations by userId and roomId

--- src/domain/interfaces/reservationRepository.ts
+++ src/domain/interfaces/reservationRepository.ts
@@ -1,7 +1,11 @@
 import { Reservation } from "../models/reservation";
+import { Room } from "../models/room";
+import { User } from "../models/user";
 
 export interface ReservationRepository {
     findAll(): Promise<Reservation[]>;
+    findByUser(user: User): Promise<Reservation[]> ;
+    findByRoom(room: Room): Promise<Reservation[]> ;
     findById(id: string): Promise<Reservation | null>;
     createReservation(reservation: Reservation): Promise<Reservation>;
     deleteReservation(id: string): Promise<void>;
